008b0cbf7256b93228495144844bf15116760443
tengxing <tengxing7452@163.com>
2017-06-27 21:58:17 +0800

    文件上传配置

--- src/main/java/cn/yjxxclub/springboot/config/MultipartConfig.java
+++ src/main/java/cn/yjxxclub/springboot/config/MultipartConfig.java
@@ -1,5 +1,14 @@
 package cn.yjxxclub.springboot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.servlet.MultipartConfigElement;
+
 /**
  * Author: 遇见小星
  * Email: tengxing7452@163.com
@@ -7,5 +16,17 @@ package cn.yjxxclub.springboot.config;
  * Time: 上午11:09
  * Describe:
  */
+@Configuration
 public class MultipartConfig {
+
+    @Autowired
+    private Environment ev;
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement(){
+        MultipartConfigFactory multipartConfigFactory = new MultipartConfigFactory();
+        multipartConfigFactory.setMaxFileSize(ev.getProperty("spring.http.multipart.max-file-size"));
+        multipartConfigFactory.setMaxRequestSize(ev.getProperty("spring.http.multipart.max-request-size"));
+        return multipartConfigFactory.createMultipartConfig();
+    }
 }
